9bd4e5caa3ea0299c08785e97a56a2dd96d2c460
mariusneo <mariusneo@gmail.com>
2012-06-19 23:51:50 +0200

    Added implementation for problem 3 from projecteuler.net website.
    Signed-off-by: mariusneo <mariusneo@gmail.com>

--- projecteuler/src/main/java/org/problems/project/euler/problem3/Problem3.java
+++ projecteuler/src/main/java/org/problems/project/euler/problem3/Problem3.java
@@ -9,7 +9,33 @@ package org.problems.project.euler.problem3;
  * 
  */
 public class Problem3 {
-	public static void main(String[] args){
+	
+	public static long calculateMaximumPrimeFactor(long number){
+		long maximumPrimeFactor = 1;
 		
+		long sqrtNumber = (long)Math.sqrt(number);
+		for (long i = 2; i< sqrtNumber;i++){
+			while (number % i == 0){
+				number = number/i;				
+			}
+			
+			if (number == 1){
+				maximumPrimeFactor = i;
+				break;
+			}
+			
+		}
+		
+		if (maximumPrimeFactor ==1){
+			maximumPrimeFactor = number;
+		}
+		
+		return maximumPrimeFactor;
+	}
+	
+	public static void main(String[] args){
+		long number= 600851475143L;
+		
+		System.out.println(calculateMaximumPrimeFactor(number));
 	}
 }
