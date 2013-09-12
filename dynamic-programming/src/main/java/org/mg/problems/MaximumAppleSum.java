4d087cabae8fd45c215b337674050b3bb035e9ca
mariusneo <mariusneo@gmail.com>
2013-09-12 13:59:18 +0200

    Added code for outputting the path used in the matrix for obtaining the maximum sum of apples.

--- dynamic-programming/src/main/java/org/mg/problems/MaximumAppleSum.java
+++ dynamic-programming/src/main/java/org/mg/problems/MaximumAppleSum.java
@@ -1,5 +1,10 @@
 package org.mg.problems;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * A table composed of N x M cells, each having a certain quantity of apples, is
  * given. You start from the upper-left corner. At each step you can go down or
@@ -18,10 +23,10 @@ public class MaximumAppleSum {
 		// Possible values : 
 		// 0 - means go left
 		// 1 - means go up
-		int[][] path =new int[apples.length][];
+		Coordinate[][] path =new Coordinate[apples.length][];
 		for (int i = 0; i < apples.length; i++) {
 			s[i] = new int[apples[i].length];
-			path[i] = new int[apples[i].length];
+			path[i] = new Coordinate[apples[i].length];
 		}
 
 		// the first row will contain the sum of the elements 
@@ -31,7 +36,7 @@ public class MaximumAppleSum {
 			s[0][j] += apples[0][j];
 			if (j > 0) {
 				s[0][j] += s[0][j - 1];
-				path[0][j] = 0;
+				path[0][j] = new Coordinate(0, j-1);
 			}
 			
 		}
@@ -43,7 +48,7 @@ public class MaximumAppleSum {
 			s[i][0] += apples[i][0];
 			if (i > 0) {
 				s[i][0] += s[i - 1][0];
-				path[i][0] = 1;
+				path[i][0] = new Coordinate(i-1, 0);
 			}
 		}
 		
@@ -56,9 +61,9 @@ public class MaximumAppleSum {
 			for (int j=1;j<s[0].length;j++){
 				s[i][j] = Math.max(s[i-1][j] + apples[i][j], s[i][j-1] + apples[i][j]);
 				if (s[i][j] - apples[i][j]== s[i-1][j] ){
-					path[i][j]=1;
+					path[i][j]= new Coordinate(i-1, j);
 				}else{
-					path[i][j]=0;
+					path[i][j]= new Coordinate(i, j-1);
 				}
 				if (i==s.length-1 || j == s[0].length - 1){
 					if (s[i][j]> maxsum){
@@ -70,11 +75,20 @@ public class MaximumAppleSum {
 			}
 		}
 		
+		System.out.println("APPLES");
+		for (int i=0;i<apples.length;i++){
+			for (int j=0;j<apples[0].length;j++){
+				System.out.format("%5d", apples[i][j]);
+			}
+			System.out.println();
+		}
+		
+		System.out.println();
 		
 		System.out.println("SUMS");
 		for (int i=0;i<s.length;i++){
 			for (int j=0;j<s[0].length;j++){
-				System.out.print(s[i][j] + " ");
+				System.out.format("%5d", s[i][j]);
 			}
 			System.out.println();
 		}
@@ -84,20 +98,81 @@ public class MaximumAppleSum {
 		System.out.println("PATHS");
 		for (int i=0;i<path.length;i++){
 			for (int j=0;j<path[0].length;j++){
-				System.out.print(path[i][j] + " ");
+				System.out.format("%10s ", path[i][j]);
 			}
 			System.out.println();
 		}
 		
+		System.out.println();
+		
+		List<Integer> pathList = new ArrayList<Integer>();
+		List<String> pathSigns = new ArrayList<String>();
+		
+		Coordinate previous = path[maxi][maxj];
+		int i = maxi;
+		int j = maxj;
+		pathList.add(apples[i][j]);
+		while(previous != null){
+			if (i==previous.x){
+				pathSigns.add(">");
+			}else{
+				pathSigns.add("V");
+			}
+			i = previous.x;
+			j = previous.y;
+			pathList.add(apples[i][j]);
+			previous = path[i][j];
+		} while(previous != null); 
+
+		Collections.reverse(pathList);
+		Collections.reverse(pathSigns);
+		
+		System.out.println("MAX PATH");
+		Iterator<Integer> pathIterator = pathList.iterator();
+		Iterator<String> pathSignIterator = pathSigns.iterator();
+		int element = pathIterator.next();
+		System.out.format("%5d", element);
+		int column = 0;
+		while (pathIterator.hasNext()){
+			element = pathIterator.next();
+			String pathSign = pathSignIterator.next();
+			if (pathSign.equals(">")){
+				column++;
+				System.out.format("%5d", element);
+			}else{
+				System.out.println();
+				int spaces = 5*(column+1);
+				System.out.format("%"+spaces+"d", element);
+			}
+		}
+		System.out.println();
+		System.out.println();
 		
 		return maxsum;
 	}
 
 	public static void main(String[] args) {
-		int[][] apples = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
+		int[][] apples = { { 1, 2, 3, 4 }, { 12, 11, 6, 9}, { 8,7,10,5 }, {4,12,6,3} };
 
 		MaximumAppleSum m = new MaximumAppleSum();
 		int sum = m.getMaxSum(apples);
 		System.out.println("Sum is : " + sum);
 	}
+}
+
+class Coordinate{
+	int x;
+	int y;
+	
+	public Coordinate(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public String toString() {
+		return "[x=" + x + ", y=" + y + "]";
+	}
+	
+	
 }
